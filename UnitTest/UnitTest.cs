c4bc6bff11b55ceef5f56203127578ac685c3ad4
Kuma-0x0 <yuta.kumagai.09@gmail.com>
2023-08-28 00:59:40 +0900

    [update]テスト修正

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -1,11 +1,11 @@
-namespace UnitTest
+namespace UnitTest;
+
+[TestClass]
+public class UnitTest1
 {
-    [TestClass]
-    public class UnitTest1
+    [TestMethod("テスト1")]
+    public void TestMethod1()
     {
-        [TestMethod("テスト1")]
-        public void TestMethod1()
-        {
-        }
+        Assert.IsTrue(true);
     }
 }
